f560b0b32834b3689045ada0c17c541a448a6d43
Jihadassi <12345678+Jihadassi@users.noreply.github.com>
2025-12-05 11:37:48 +0100

    Del

---
+++ src/main/java/com/example/bilabonnementeks/BilabonnementEksApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnementeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilabonnementEksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilabonnementEksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bilabonnementeks/BilabonnementEksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnementeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilabonnementEksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
